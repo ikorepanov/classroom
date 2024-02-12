6885c2f5815efbd6749dd9e5166e5fd62eb60ede
Ilia Korepanov <ikorepanov@gmail.com>
2024-02-12 14:56:06 +0700

    Add generate_seating_order on the previous developed algorithm

---
+++ classroom/seats/atempt.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+
+def generate_seating_order(number_of_linear_arrays,
+                           number_of_seating_arrangements):
+
+    indices = np.arange(number_of_linear_arrays)
+    mentioned_indices = []
+    count = 0
+    last_index = None
+    seating_order = []
+
+    while True:
+        print('Цикл начинается здесь')
+        for i in range(len(indices)):
+            if count == number_of_seating_arrangements:
+                break
+            probabilities = [1 / (len(indices)-i) for j in range(len(indices))]
+            for index in mentioned_indices:
+                probabilities[index] = 0
+            array_probs = np.array(probabilities)
+            index = np.random.choice(indices, p=array_probs)
+            if index == last_index and count % number_of_linear_arrays == 0:
+                print('Прерывание!', index)
+                break
+            mentioned_indices.append(index)
+            count += 1
+            print('РЕЗУЛЬТАТ: ', index)
+            seating_order.append(index)
+            # print('Количество операций генерации индекса: ', count)
+        last_index = index
+        mentioned_indices.clear()
+        if count == number_of_seating_arrangements:
+            break
+    return seating_order
+
+
+number_of_linear_arrays = 6
+number_of_seating_arrangements = 9
+
+print(generate_seating_order(number_of_linear_arrays,
+                             number_of_seating_arrangements))
--- classroom/seats/utils2.py
+++ classroom/seats/utils2.py
@@ -1,32 +1,32 @@
 import numpy as np
 
-axis = 6
+number_of_linear_arrays = 6
 
-arr = np.arange(axis)
+indices = np.arange(number_of_linear_arrays)
 
-used_indices = []
+mentiones_indices = []
 count = 0
-number_of_arrangements = 9
+number_of_seating_arrangements = 9
 last_index = None
 
 while True:
     print('Цикл начинается здесь')
-    for i in range(len(arr)):
-        if count == number_of_arrangements:
+    for i in range(len(indices)):
+        if count == number_of_seating_arrangements:
             break
-        probabilities = [1 / (len(arr)-i) for j in range(len(arr))]
-        for index in used_indices:
+        probabilities = [1 / (len(indices)-i) for j in range(len(indices))]
+        for index in mentiones_indices:
             probabilities[index] = 0
         array_probs = np.array(probabilities)
-        index = np.random.choice(arr, p=array_probs)
-        if index == last_index and count % axis == 0:
+        index = np.random.choice(indices, p=array_probs)
+        if index == last_index and count % number_of_linear_arrays == 0:
             print('Прерывание!', index)
             break
-        used_indices.append(index)
+        mentiones_indices.append(index)
         count += 1
         print('РЕЗУЛЬТАТ: ', index)
         # print('Количество операций генерации индекса: ', count)
     last_index = index
-    used_indices.clear()
-    if count == number_of_arrangements:
+    mentiones_indices.clear()
+    if count == number_of_seating_arrangements:
         break
